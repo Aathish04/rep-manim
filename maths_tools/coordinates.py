17facc0824ff59bb89fe08b99ef5a677c5ab2852
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2019-12-31 09:52:04 +0530

    Added Maths Tools:spherical_to_cartesian

---
+++ maths_tools/coordinates.py
@@ -0,0 +1,7 @@
+#Some tools that are useful for coordinate manipulation and whatnot.
+
+def spherical_to_cartesian(pol_ang,azim_ang,radius): #This function converts given spherical coordinates (theta, phi and radius) to cartesian coordinates.
+        return np.array((radius*np.sin(pol_ang) * np.cos(azim_ang),
+                            radius*np.sin(pol_ang) * np.sin(azim_ang),
+                            radius*np.cos(pol_ang))
+                            )
