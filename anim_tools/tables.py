1bb94128536c00fe846e3a4958baf9e4f4345588
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-01-05 11:08:12 +0530

    Added support for TextMobjects and Text. Solved length checking issue. Must address Raw String Field bug.

--- anim_tools/tables.py
+++ anim_tools/tables.py
@@ -1,10 +1,12 @@
 from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.mobject.svg.tex_mobject import TexMobject
+from manimlib.mobject.svg.tex_mobject import TextMobject
+from manimlib.mobject.svg.text_mobject import Text
 from manimlib.mobject.geometry import Line
 from manimlib.constants import *
+
 class Table(VGroup):
-#TODO : Add proper length checking to avoid smaller than useful vertical lines when dealing with TeX
-# Use CONFIG instead of directly passing parameters to function?
+#TODO: Use CONFIG instead of directly passing parameters to function? Table Element retrieval.
 
     def get_table(tabledict:dict,buff_length=0.3,line_color=WHITE,text_color=WHITE):
         
@@ -19,25 +21,38 @@ class Table(VGroup):
         
         fields=list(tabledict.keys()) #Since the data is recieved as a dict, the keys will be the fields
 
-        cell_length=TexMobject(max(fields + flatten(tabledict.values()), key=len)).get_width()+2*buff_length #The length of a record/field of max length is the base cell size
-        cell_height=TexMobject(max(fields + flatten(tabledict.values()), key=len)).get_height()+2*buff_length
-       
+        #the for loop below checks that every field and record is of a valid type and converts if need be:
+        for fieldnum in range(len(fields)):
+            
+            if isinstance(fields[fieldnum],TexMobject)==False and isinstance(fields[fieldnum],TextMobject)==False and isinstance(fields[fieldnum],Text)==False :
+                fields[fieldnum]=TexMobject(fields[fieldnum])
+
+            for recordnum in range(0,len(tabledict[fields[fieldnum]])):
+                if isinstance(tabledict[fields[fieldnum]][recordnum],TexMobject)==False and isinstance(tabledict[fields[fieldnum]][recordnum],TextMobject)==False and isinstance(tabledict[fields[fieldnum]][recordnum],Text)==False:
+                    tabledict[fields[fieldnum]][recordnum]=TexMobject(tabledict[fields[fieldnum]][recordnum])
+                else:
+                    continue
+
+        cell_length=( max(fields + flatten(tabledict.values()), key=lambda m:m.get_width()) ).get_width() + 2*buff_length #The length of a record/field of max length is the base cell size
+        cell_height=( max(fields + flatten(tabledict.values()), key=lambda m:m.get_height()) ).get_height()+ 2*buff_length
+        
         #The first position is set like so.
+        
         field_position=[ (cell_length-TexMobject(fields[0]).get_width())/2 + TexMobject(fields[0]).get_width()/2, 0,0 ] #The initial position of the first field. This is 
+        
         #NOTE: Coordinates of TexMobjects in Manim are taken from centre, not top-right. Adjustments have been made.
         
-        total_table_width=(cell_length-TexMobject(fields[0]).get_width())/2
+        total_table_width=(cell_length*len(fields)) #The remaining width and height will be successively added
 
-        total_table_height=cell_height*(len(tabledict[max(tabledict.keys(),key=len)])+1)
+        total_table_height=cell_height*(len(max(tabledict.values(),key=len))+1)#while drawing the Mobjects to the screen. +1 is added to account for headings. 
 
-        for n in range(len(fields)):
+        for n in range(0,len(fields)):
             
-            field=TexMobject(fields[n])
-            
+            field=fields[n]
             field_length=field.get_width() #This is the length that the actual field name will take up on-screen
             
             if n+1<len(fields): #This gets the nxt field if it exists and chooses an empty TexMobject if it doesn't
-                next_field=TexMobject(fields[n+1])
+                next_field=(fields[n+1])
             else:
                 next_field=TexMobject("")
             
@@ -55,7 +70,6 @@ class Table(VGroup):
             
             #next_field_length/2 is added to account for the fact that coordinates are taken from centre and not left edges.
 
-            total_table_width+=field_length+space_to_leave/2
             table.add(field)
             field_position=field.get_right()+(space_to_leave,0,0)
         
@@ -65,16 +79,16 @@ class Table(VGroup):
             
             if recordlist!=[]:
 
-                record_position=[table[keynum].get_center()[0], -((cell_height-TexMobject(fields[keynum]).get_height())/2 + TexMobject(fields[keynum]).get_height()/2 + cell_height ),0]
+                record_position=[table[keynum].get_center()[0], -((cell_height-fields[keynum].get_height())/2 + fields[keynum].get_height()/2 + cell_height ),0]
                 
                 #the record position is set to be the [center of the field it belongs to, buffer space above the record + centered height of the record, 0  ]
 
                 for recordnum in range(len(recordlist)):    # for each record for
-                    record=TexMobject(recordlist[recordnum]) # the selected field
+                    record=recordlist[recordnum] # the selected field
                  
                     
                     if recordnum+1<len(recordlist): #This gets the nxt record if it exists and chooses an empty TexMobject if it doesn't
-                        next_record=TexMobject(recordlist[recordnum+1])
+                        next_record=recordlist[recordnum+1]
                     else:
                         next_record=TexMobject("")
                 
