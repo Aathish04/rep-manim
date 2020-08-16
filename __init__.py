775f29e010c24e4cb4121a265ed0f7baba6fc0aa
Aathish Sivasubrahmanian <aathish04@gmail.com>
2020-08-16 18:22:16 +0530

    Made Sanim an installable package.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .anim_tools.methods import *
+from .anim_tools.shading import *
+from .anim_tools.tables import *
+
+from .maths_tools.coordinates import *
+
+from .sci_objects.atoms import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="sanim",
+    version="0.0.0-3b1b",
+    description="Generic Science tools for Manim",
+    license="MIT",
+)
