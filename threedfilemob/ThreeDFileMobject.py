eefa63fd370751001926ec46d929170c8329cff7
Aathish Sivasubrahmanian <aathish04@gmail.com>
2021-09-08 15:39:00 +0530

    Acknowledge existence of colours and groups.

--- threedfilemob/ThreeDFileMobject.py
+++ threedfilemob/ThreeDFileMobject.py
@@ -3,11 +3,13 @@ from os import path
 
 
 class OBJVMobject(VGroup):
-    # TODO: Colours; Free-form curves/surfaces
+    # TODO: Colours; Free-form curves/surfaces; smoothing groups
 
     def _parse_obj_file(self):
         isint = lambda s: s.isdigit() or (s.startswith("-") and s[1:].isdigit())
 
+        groups = None  # Not all obj files specify groups. Set default group to None
+        material = None  # Some faces have no material specified. WTH?
         for line in self.textlines:
             if line.startswith("#") or len(line.strip()) == 0:
                 continue
@@ -19,26 +21,36 @@ class OBJVMobject(VGroup):
                 self.vertex_normals.append(list(map(float, linedata[1:4])))
             elif linedata[0] == "vt":
                 self.texture_vertices.append(list(map(float, linedata[1:4])))
+            elif linedata[0] == "g":
+                groups = (
+                    linedata[1:] if len(linedata) > 1 else None
+                )  # If no group names are mentioned use None.
+            elif linedata[0] == "usemtl":
+                material = linedata[1]
             elif linedata[0] == "f":
-                face_vertices = [
+                face_info = {"group": groups, "usematerial": material}
+                face_info["vertices"] = [
                     {
                         ("vertex", "texture_vertex", "vertex_normal")[i]: (
                             self.vertices,
                             self.texture_vertices,
                             self.vertex_normals,
-                        )[i][int(index) - 1 if int(index) >= 1 else int(index)]
+                        )[i][
+                            int(index) - 1 if int(index) >= 1 else int(index)
+                        ]  # Indexing starts from 1. Reverse indexing also possible.
                         if isint(index)
                         else None
                         for i, index in enumerate(text_vertex_data.split("/"))
                     }
                     for text_vertex_data in linedata[1:]
                 ]
-                self.faces_vertices.append(face_vertices)
-            
+                self.faces_info.append(face_info)
 
     def _build_faces(self):
-        for face_vertices in self.faces_vertices:
-            vertex_list = [face_vertex["vertex"] for face_vertex in face_vertices]
+        for face_info in self.faces_info:
+            vertex_list = [
+                face_vertex["vertex"] for face_vertex in face_info["vertices"]
+            ]
             face = ThreeDVMobject().set_points_as_corners(
                 vertex_list + [vertex_list[0]]
             )
@@ -52,7 +64,7 @@ class OBJVMobject(VGroup):
         self.vertex_normals = []
         self.texture_vertices = []
 
-        self.faces_vertices = []
+        self.faces_info = []
         with open(fp, "r") as f:
             self.textlines = f.readlines()
 
@@ -72,8 +84,17 @@ class Test(ThreeDScene):
         self.set_camera_orientation(
             phi=-45 * DEGREES, theta=-135 * DEGREES, gamma=-55 * DEGREES
         )
-        filename = "cow.obj"
+        filename = "coloured/airboat/airboat.obj"
         model = OBJVMobject(
-            path.abspath(path.dirname(__file__) + path.sep + "ref_models" + path.sep+ filename)
-        ).shift(UP*0.5)
+            path.abspath(
+                path.dirname(__file__) + path.sep + "ref_models" + path.sep + filename
+            )
+        ).shift(UP * 0.5)
+        with open(
+            "/Users/aathishs/Projects/Python/ManimStuff/rep-manim/threedfilemob/ThreeDFileMobject.json",
+            "w",
+        ) as f:
+            import json
+
+            json.dump(model.faces_info, f)
         self.add(model)
